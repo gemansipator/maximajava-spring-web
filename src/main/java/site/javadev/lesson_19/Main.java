b6f1b2d2841e1f0bd480ce41df79f17f571937a4
gemansipator <gemansipator@gmail.com>
2024-11-01 15:26:37 +0300

    init

---
+++ src/main/java/site/javadev/lesson_19/Main.java
@@ -0,0 +1,21 @@
+package site.javadev.lesson_19;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Загружаем контекст из файла конфигурации
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Получаем bean player и запускаем метод play()
+        RadioPlayer player1 = context.getBean("player", RadioPlayer.class);
+        player1.play();
+
+        // Проверка на prototype scope: player1 и player2 должны быть разными объектами
+        RadioPlayer player2 = context.getBean("player", RadioPlayer.class);
+        System.out.println(player1 == player2); // Должно вывести false
+
+        context.close();
+    }
+}
